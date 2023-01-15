a0b8f49955ca0f2eb9ad00577b13700b4aaf5a0c
adamkoro <adam.koronics@gmail.com>
2023-01-15 17:20:32 +0100

    feat: :zap: Basic database functions

--- auth/pkg/db/db.go
+++ auth/pkg/db/db.go
@@ -3,6 +3,8 @@ package db
 import (
 	"database/sql"
 	"fmt"
+
+	_ "github.com/lib/pq"
 )
 
 // Create a database connection url
@@ -19,4 +21,22 @@ func Connect(host, username, password, database string, port int) (*sql.DB, erro
 		return nil, err
 	}
 	return db, nil
+}
+
+// Close the database connection
+func Close(db *sql.DB) error {
+	err := db.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Ping the database connection
+func Ping(db *sql.DB) error {
+	err := db.Ping()
+	if err != nil {
+		return err
+	}
+	return nil
 }

fe2faf9d2fb5a1283816a7db7715775b968c75a1
adamkoro <adam.koronics@gmail.com>
2023-01-16 21:26:24 +0100

    feat: JWT generate and validator

--- auth/pkg/jwt/jwt.go
+++ auth/pkg/jwt/jwt.go
@@ -1,1 +1,34 @@
 package jwt
+
+import (
+	"demo-k8s-auth/model"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+// Create jwt token
+func Create(username string) (string, error) {
+	claims := model.TokenClaims{
+		Username: username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(jwt.TimeFunc().Add(24 * 7 * 3600 * 1000)),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte("secret"))
+}
+
+// Parse jwt token
+func Parse(tokenString string) (*model.TokenClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &model.TokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte("secret"), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if claims, ok := token.Claims.(*model.TokenClaims); ok && token.Valid {
+		return claims, nil
+	}
+	return nil, err
+
+}

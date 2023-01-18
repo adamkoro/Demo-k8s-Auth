e49ed547a3e869217d6a171e694cdfcf6b0c9d91
adamkoro <adam.koronics@gmail.com>
2023-01-18 20:32:26 +0100

    feat: :fire: New user struct

--- auth/model/user.go
+++ auth/model/user.go
@@ -1,6 +1,15 @@
 package model
 
 type User struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	ID        uint32 `json:"id"`
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+	Email     string `json:"email"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+	DeletedAt string `json:"deleted_at"`
+	Phone     string `json:"phone"`
+	Salt      string `json:"salt"`
 }

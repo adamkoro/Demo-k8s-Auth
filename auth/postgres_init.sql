e6f61370213a63445458d90668c7932a8b1db666
adamkoro <adam.koronics@gmail.com>
2023-01-18 20:30:30 +0100

    chore: Init sql

---
+++ auth/postgres_init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    firstName VARCHAR(255),
+    lastName VARCHAR(255),
+    email VARCHAR(255) NOT NULL,
+    phone VARCHAR(255),
+    password VARCHAR(255) NOT NULL,
+    Salt VARCHAR(255),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP
+);

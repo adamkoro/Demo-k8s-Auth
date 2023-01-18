24dad82556a87669ee450f094475e7bb3fc86dcc
adamkoro <adam.koronics@gmail.com>
2023-01-18 20:32:45 +0100

    feat: :fire: New typo of error message

--- auth/model/message.go
+++ auth/model/message.go
@@ -8,3 +8,15 @@ type HealthResponse struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
 }
+
+type StandardError struct {
+	Error string `json:"error"`
+}
+
+type UnauthorizedError struct {
+	Error string `json:"error"`
+}
+
+type InternalServerError struct {
+	Error string `json:"error"`
+}

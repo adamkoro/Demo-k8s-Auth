3edc7c86125c5f77f7ed9ec0a04f1b5c4c69ff17
adamkoro <adam.koronics@gmail.com>
2023-01-18 20:31:39 +0100

    feat: New functions

--- auth/pkg/jwt/jwt.go
+++ auth/pkg/jwt/jwt.go
@@ -4,6 +4,7 @@ import (
 	"demo-k8s-auth/model"
 
 	"github.com/golang-jwt/jwt/v4"
+	"github.com/google/uuid"
 )
 
 // Create jwt token
@@ -30,5 +31,40 @@ func Parse(tokenString string) (*model.TokenClaims, error) {
 		return claims, nil
 	}
 	return nil, err
+}
 
+// Refresh jwt token
+func Refresh(tokenString string) (string, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &model.TokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte("secret"), nil
+	})
+	if err != nil {
+		return "", err
+	}
+	if claims, ok := token.Claims.(*model.TokenClaims); ok && token.Valid {
+		claims.ExpiresAt = jwt.NewNumericDate(jwt.TimeFunc().Add(24 * 7 * 3600 * 1000))
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		return token.SignedString([]byte("secret"))
+	}
+	return "", err
+}
+
+// Validate jwt token
+func Validate(tokenString string) bool {
+	token, err := jwt.ParseWithClaims(tokenString, &model.TokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte("secret"), nil
+	})
+	if err != nil {
+		return false
+	}
+	if _, ok := token.Claims.(*model.TokenClaims); ok && token.Valid {
+		return true
+	}
+	return false
+}
+
+// Generate unique uuid
+func GenerateID() uint32 {
+	id := uuid.New().ID()
+	return id
 }
